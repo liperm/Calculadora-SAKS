1508259c41f6db77fa08e0a919805e520500a4f1
Felipe Ribeiro Machado <7429061@univali.br>
2022-07-25 14:42:55 -0300

    tratamento de inputs do usuário

--- calculadora.js
+++ calculadora.js
@@ -33,17 +33,25 @@ function valorTotal(valorTotalInvestido, rendimentoTotal, impostoDeRenda){
     return (valorTotalInvestido + rendimentoTotal - impostoDeRenda)
 }
 
-function onlynumber(evt) {
-    var theEvent = evt || window.event;
-    var key = theEvent.keyCode || theEvent.which;
-    key = String.fromCharCode( key );
-    //var regex = /^[0-9.,]+$/;
-    var regex = /^[0-9.]+$/;
-    if( !regex.test(key) ) {
-       theEvent.returnValue = false;
-       if(theEvent.preventDefault) theEvent.preventDefault();
+ function formatarMoeda(input) {
+    var elemento = input;
+    var valor = elemento.value;
+    
+
+    valor = valor + '';
+    valor = parseInt(valor.replace(/[\D]+/g, ''));
+    valor = valor + '';
+    valor = valor.replace(/([0-9]{2})$/g, ",$1");
+
+    if (valor.length > 6) {
+        valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
     }
- }
+
+    elemento.value = valor;
+    if(valor == 'NaN') elemento.value = '';
+    
+}
+
 
 /*var juros = juros(10000.00, 1/100, 12, 1000.00)
 var imposto = imposto(juros, 10*30)
